d9954dc5370d13ae8e02c489933ea4170839425f
ofeibaduwilliam <ofeibaduwilliam@gmail.com>
2025-09-18 18:46:03 +0000

    The while loop and the do while loop

---
+++ doWhile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int main(){
+    int index = 1;
+    do{
+        printf("%d\n", index);
+        index ++;
+    }while(index <=5);
+
+
+
+    return 0;
+}
---
+++ whileLoops.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int main(){
+    int index = 1;
+    while(index <=5){
+        printf("%d\n", index);
+        index ++;
+    }
+
+
+
+    return 0;
+}
