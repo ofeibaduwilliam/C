c5561f3290d31c1bf03c592129d156046227c4aa
ofeibaduwilliam <ofeibaduwilliam@gmail.com>
2025-09-18 14:23:10 +0000

    The Better Calculator has been added to the process

--- if.c
+++ if.c
@@ -7,6 +7,9 @@ int max(int num1, int num2, int num3){
     }else if(num2 >= num1 && num2 >= num3) {
         result = num2;
     }
+    else {
+        result = num3;
+    }
     return result;
 }
 
