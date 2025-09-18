a5c5062b489f8578b1efe631877b49e286d4d193
ofeibaduwilliam <ofeibaduwilliam@gmail.com>
2025-09-18 20:04:58 +0000

    Two dimensional arrays

---
+++ 2dArrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int main(){
+    int nums[3][2] = {
+        {1,2},
+        {3,4},
+        {5,6}
+    };
+    printf("%d", nums[1][0]);
+
+    return 0;
+}
