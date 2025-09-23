5932645214164d9821e9060ed71c36a5820af67b
ofeibaduwilliam <ofeibaduwilliam@gmail.com>
2025-09-23 23:54:04 +0000

    Lab 3 try work

---
+++ lab3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Examples of programming languages are:\n\t1. Java\n\t2. C++\n\"What other language do you know?\"");
+    return 0;
+}
